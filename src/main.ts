12affd7d42632e6f7b427e9692f1fbd57c9d38c0
Diego Figueroa <diego.figueroa@aurora.dev>
2023-10-11 17:11:37 -0400

    build: update build to node16 standards

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,23 @@ import { getActionInputs } from "./utils/get-action-inputs";
 export const run = async () => {
   try {
     const helper = core.getInput("helper", { required: true });
+    core.debug(`Helper: ${helper}`);
+
     const {
       [camelCase(helper)]: method,
       [upperFirst(camelCase(helper))]: HelperInterface,
     } = await import(`./helpers/${helper}`);
+    core.debug(`Interface: ${HelperInterface}`);
+
     const requiredInputs = HelperInterface
       ? Object.keys(new HelperInterface())
       : [];
+    core.debug(`Required Inputs: ${requiredInputs}`);
+
     const actionInputs = getActionInputs(requiredInputs);
+
     const output = await method(actionInputs);
+    core.debug(`Output: ${output}`);
     core.setOutput("output", output);
   } catch (error) {
     core.setFailed(error as Error);
